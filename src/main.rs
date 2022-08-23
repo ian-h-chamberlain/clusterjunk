fec24061c3b51c0fcea4cad58a30a837fa7cb5ad
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2022-08-22 20:20:41 -0400

    cruft update

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 // disable console on windows for release builds
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::io::Cursor;
+
 use bevy::prelude::*;
+use bevy::window::WindowId;
+use bevy::winit::WinitWindows;
 use bevy::DefaultPlugins;
 use bevy_rapier2d::prelude::*;
+use winit::window::Icon;
 
 use clusterjunk::GamePlugin;
 
@@ -16,14 +21,29 @@ fn main() {
             width: 800.,
             height: 600.,
             title: "Clusterjunk!".to_string(),
+            canvas: Some("#bevy".to_owned()),
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
-        .add_plugin(GamePlugin);
+        .add_plugin(GamePlugin)
+        .add_startup_system(set_window_icon);
 
     #[cfg(feature = "dev")]
     app.add_plugin(RapierDebugRenderPlugin::default());
 
     app.run();
+}
+
+// Sets the icon on windows and X11
+fn set_window_icon(windows: NonSend<WinitWindows>) {
+    let primary = windows.get_window(WindowId::primary()).unwrap();
+    let icon_buf = Cursor::new(include_bytes!("../assets/textures/bevy.png"));
+    if let Ok(image) = image::load(icon_buf, image::ImageFormat::Png) {
+        let image = image.into_rgba8();
+        let (width, height) = image.dimensions();
+        let rgba = image.into_raw();
+        let icon = Icon::from_rgba(rgba, width, height).unwrap();
+        primary.set_window_icon(Some(icon));
+    };
 }
